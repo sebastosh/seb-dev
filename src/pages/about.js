90992e1886aed19f51aaef1357d3fa2d20e9f71d
sebastosh <sebastosh@gmail.com>
2019-10-19 13:00:38 -0400

    header style

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,34 +8,39 @@ import resume from "../images/SebastienSanzdeSantamariaResume2019.pdf"
 export default ({ data }) => (
   <Layout>
     <div className="container">
-      <h1>About {config.userName}</h1>
-      <p>{config.about}</p>
+      <div className="elevator"><h1>About {config.userName}</h1></div>
+
+    <section className="section"  >
+    <p>{config.about}</p>
 
    
-        <div id="skills">
-            <h1>Skills</h1>
-            
-        
-                {config.skills.map(tag => (
-                  <div className="skill">{tag}</div>
-                ))}
-          
-            </div>
-     
+<div id="skills">
+    <h1>Skills</h1>
+    
 
-          <div className="resume">
-            <h1>Resume</h1>
-            <a href={resume} title="Resume">
-              <img
-                src={fileicon}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="footer-img"
-                alt="Resume"
-              />
-            </a>
-          </div>
-      </div>
+        {config.skills.map(tag => (
+          <div className="skill">{tag}</div>
+        ))}
+  
+    </div>
+
+
+  <div className="resume">
+    <h1>Resume</h1>
+    <a href={resume} title="Resume">
+      <img
+        src={fileicon}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-img"
+        alt="Resume"
+      />
+    </a>
+  </div>
+    </section>
+
+
+    </div>
   
   </Layout>
 )
