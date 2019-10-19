7113c7c364d838f997a1d8e89de7460c07d77000
sebastosh <sebastosh@gmail.com>
2019-10-19 00:23:52 -0400

    footer clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,16 +11,9 @@ export default class Footer extends Component {
     return (
       <footer className="footer container">
         <div>
-          {/* <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Ko-Fi
-          </a> 
-          <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Patreon
-          </a>*/}
+   
           <Link to="/contact">Contact</Link>
-          {/* <a href="https://sebastiensanzdesantamaria.net/rss.xml" target="_blank" rel="noopener noreferrer">
-            RSS
-          </a> */}
+    
         </div>
         <div>
           <a href="https://github.com/sebastosh" title="Seb on GitHub">
